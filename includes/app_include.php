77129f2211ecb78fda2300e61f4da6409d286f70
Moreno <moreno.zolfo@gmail.com>
2020-04-11 15:13:05 -0400

    c

--- includes/app_include.php
+++ includes/app_include.php
@@ -1,9 +1,17 @@
 <?php
-$db_Host = "localhost";
+
+$db_Host = "colab1.cibio.unitn.it";
 $db_User = "covid_user";
 $db_Pass = "***REMOVED***";
-$db_Name = 'covid';
+$db_Name = 'covid'; 
+$db_Port = 33006; 
 
+
+#$db_Host = "localhost";
+#$db_User = "debian-sys-maint";
+#$db_Pass = "uvMB19ny5KOciZlt";
+#$db_Name = 'covid';  
+#$db_Port = 3306; 
 
 function mysql_query($q){return Database::query($q);}
 function mysql_fetch_array($q){return mysqli_fetch_array($q);}
@@ -15,27 +23,28 @@ class Database
 {
 	public static $link;
 
-	public static function tdbConnect($db_Host,$db_User, $db_Pass,$db_Name)
-	{
-		self::$link=mysqli_connect($db_Host,$db_User, $db_Pass,$db_Name);
+	public static function tdbConnect($db_Host,$db_User, $db_Pass,$db_Name,$db_Port)
+	{	
+
+		self::$link=mysqli_connect($db_Host,$db_User, $db_Pass,$db_Name,$db_Port);
 	}
 	
 	public static function tdbClose()
 	{
-		mysql_close(self::$link);
+		mysqli_close(self::$link);
 	}
 
 	public static function query($query)
 	{ 
-		$QR = mysql_query(self::$link,$query);
-		if(mysql_error(self::$link))
+		$QR = mysqli_query(self::$link,$query);
+		if(mysqli_error(self::$link))
 			echo mysqli_error(self::$link);
 		return $QR;
 	}
 }
 
 
-Database::tdbConnect($db_Host,$db_User, $db_Pass,$db_Name);
+Database::tdbConnect($db_Host,$db_User, $db_Pass,$db_Name,$db_Port);
 Database::query('SET NAMES utf8');
 
 ?>
