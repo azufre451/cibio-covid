c99cb8f15be6d1974aef39d4e6edc97651a5d250
Moreno <moreno.zolfo@gmail.com>
2020-05-08 04:34:10 +0200

    modified backend

--- backend/extr_data_analysis.py
+++ backend/extr_data_analysis.py
@@ -7,6 +7,8 @@ import glob
 import os
 import sys
 from locale import atof, setlocale, LC_NUMERIC, LC_ALL
+from itertools import islice
+import pandas as pd
 setlocale(LC_NUMERIC,('it_IT','UTF-8'))
 
 
@@ -24,6 +26,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data_folder', help='his is the folder containing all the Analisi Excell files. One file per plate.', required=True)
 parser.add_argument('--control_wells', help="manual setup for control wells in the plate (list, space separated. Esample: A01 A02 A03 ...)", default=[], nargs='+')
 
+
+parser.add_argument('--kit', default="bosphore")
 parser.add_argument('--kf', action='store_true',help='Equals to --control_wells A01 A02 A12 D04 D08')
 parser.add_argument('--old_plates', action='store_true',help='Equals to --control_wells H01 A01 A02 A05 A08 A12')
 
@@ -38,6 +42,8 @@ parser.add_argument('--platename_from_file',action='store_true', help='allows to
 args = parser.parse_args()
 
 control_samples = ['NTC','PK','PE','BE','55555','77777','11111111','33333','20202020','25252525','27272727', 'pcr NEG','pcr POS']
+
+fluorophores=["FAM","HEX","Cy5"]
 
 if args.kf:
 	ListOfControlWells=['A01','A02','A12','D04','D08']
@@ -61,7 +67,10 @@ mydb = mysql.connector.connect(
 
 
 samplesToAdd=[]
+curves2add=[]
 samplesToCheck=[]
+
+well2barcode={}
 mycursor = mydb.cursor()
 
 for analFile in glob.glob(args.data_folder+'/*.xls*'):
@@ -90,6 +99,7 @@ for analFile in glob.glob(args.data_folder+'/*.xls*'):
 			fline=False
 			continue
 
+		well_nozero= str(row[0].value)
 		well= str(row[1].value)
 			#target= str(row[3].value)
 		barcode= str(row[3].value)
@@ -127,10 +137,9 @@ for analFile in glob.glob(args.data_folder+'/*.xls*'):
 			mycursor.execute(sql, (barcode,))
 			mycursor.fetchone()
 			if(mycursor.rowcount > 0):
-				print(well,barcode)
 						#print("OK", plateName,barcode,well,val_cy5,val_fam,val_hex,auto_result,final_result )
-				samplesToAdd.append ( (plateName, plateDate,barcode,well,val_cy5,val_fam,val_hex,auto_result,final_result, is_control,batch_kf))
-
+				samplesToAdd.append ( (plateName, plateDate,barcode,well,val_cy5,val_fam,val_hex,auto_result,final_result, is_control,batch_kf,args.kit))
+				well2barcode[well_nozero] = barcode
 			else:
 				
 				if str(barcode) != "0":
@@ -140,17 +149,46 @@ for analFile in glob.glob(args.data_folder+'/*.xls*'):
 				#print("NOK")
 
 
-print("Samples to Check: ",len(samplesToCheck))
-for k in samplesToCheck:
-	print(k)
 
-#print("Samples to add: ",len(samplesToAdd))
-#for k in samplesToAdd:
-#	print(k)
+	print("Samples to Check: ",len(samplesToCheck))
+	for k in samplesToCheck:
+		print(k)
 
-sql = 'INSERT IGNORE INTO pcr_plates (plate, data_pcr, barcode, well, Cy5, FAM, HEX, esito_automatico, esito_pcr, isControl,batch_kf) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+	
+
+	for fluorophore in fluorophores:
+
+		try:
+			sheetFam = wb_obj["Curve "+fluorophore]
+		except:
+			print("Error in finding Curve "+fluorophore+" tab", analFile )
+			sys.exit(0)
+		
+		data = sheetFam.values
+		cols = next(data)[1:]
+		data = list(data)
+		idx = [r[0] for r in data]
+		data = (islice(r, 1, None) for r in data)
+		df = pd.DataFrame(data, index=idx, columns=cols).set_index('Cycle')
+		
+
+		for wellTo in df.columns:
+			if wellTo in well2barcode:
+				barcode = well2barcode[wellTo]
+				wellDB = wellTo[0]+wellTo[1:].zfill(2)
+				curveString = ','.join(map(str,list(df[wellTo])))
+				curves2add.append( (plateName,wellDB,fluorophore,curveString) )
+	
+
+ 
+
+sql = 'INSERT IGNORE INTO pcr_plates (plate, data_pcr, barcode, well, Cy5, FAM, HEX, esito_automatico, esito_pcr, isControl,batch_kf,kit) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
 #print(sql)
 mycursor.executemany(sql, samplesToAdd)
+
+
+sql = 'INSERT INTO curves (plate, well, fluorophore, curve) VALUES (%s,%s,%s,%s)'
+mycursor.executemany(sql, curves2add)
 
 
 
