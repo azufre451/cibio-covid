bbd8f54bd9323deab05f4689b903846a934e98f5
www-data <moreno.zolfo@gmail.com>
2020-05-12 17:26:33 +0000

    appinclude

--- includes/app_include.php
+++ includes/app_include.php
@@ -1,18 +1,8 @@
 <?php
 error_reporting(E_ERROR);
-$db_Host = "colab1.cibio.unitn.it";
-$db_User = "covid_user";
-$db_Pass = "***REMOVED***";
-$db_Name = 'covid'; 
-$db_Port = 33006; 
+include('includes/conf.php');
 
 $appVersion = "1.0.5.2";
-
-//$db_Host = "localhost";
-//$db_User = "debian-sys-maint";
-//$db_Pass = "uvMB19ny5KOciZlt";
-//$db_Name = 'covid';  
-//$db_Port = 3306; 
 
 function mysql_query($q){return Database::query($q);}
 function mysql_fetch_array($q){return mysqli_fetch_array($q);}
@@ -44,8 +34,7 @@ class Database
 	}
 }
 
-$OPT_refertUSR = 'dma';
-$OPT_refertPWD = '***REMOVED***';
+
 $fluor2colors	=	array('FAM' => '#36a723','HEX' => '#2e69d5','Cy5' => '#b71656','TRed' => '#ff2f0e');
 $plotOptions	=	array('bosphore'=> array(
 								'max_pcr_cycles' => 40,
